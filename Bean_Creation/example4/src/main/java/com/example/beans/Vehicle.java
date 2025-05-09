518b789f3ddd57e293aedd61a4a6f65428e9d282
Ankith M <reachankit2013@gmail.com>
2025-05-09 21:59:06 +0530

    feat: bean wiring with method parameters

---
+++ Bean_Creation/example4/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,22 @@
+package com.example.beans;
+
+public class Vehicle {
+
+    public Vehicle(){ // constructor
+        System.out.println("Vehicle bean has been created!");
+    }
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Vehicle name is "+ name;
+    }
+}
