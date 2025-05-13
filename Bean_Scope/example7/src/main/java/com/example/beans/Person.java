ea986b842528cb4b271bb0c4d4fc5e19f391d3e6
Ankith M <reachankit2013@gmail.com>
2025-05-13 22:51:49 +0530

    feat: example on singleton bean scope

--- Bean_Scope/example7/src/main/java/com/example/beans/Person.java
+++ Bean_Scope/example7/src/main/java/com/example/beans/Person.java
@@ -1,4 +1,23 @@
 package com.example.beans;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component(value="person")
+@Scope(BeanDefinition.SCOPE_SINGLETON)
 public class Person {
+    private String name = "John";
+
+    public Person() {
+        System.out.println("Person created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Bean_Scope/example7/src/main/java/com/example/main/Example7.java
+++ Bean_Scope/example7/src/main/java/com/example/main/Example7.java
@@ -1,4 +1,22 @@
 package com.example.main;
 
+import com.example.beans.Person;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/*
+In Singleton Bean, multiple beans can be created from same object
+but only one instance will be returned.
+* */
 public class Example7 {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person p1 = context.getBean(Person.class);
+        Person p2 = context.getBean("person", Person.class);
+        System.out.println("The hashcode for object p1 is " + p1.hashCode());
+        System.out.println("The hashcode for object p2 is " + p2.hashCode());
+        if(p1.hashCode()==p2.hashCode()){
+            System.out.println("Both objects are referring the same bean instance");
+        }
+    }
 }
