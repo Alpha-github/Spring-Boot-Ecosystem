a907dc7028f2c52ac16baa1f5e45a9803828de5b
Ankith M <reachankit2013@gmail.com>
2025-05-15 23:14:18 +0530

    feat: EAGER and LAZY Instantiation

--- Bean_Scope/example7/src/main/java/com/example/beans/Person.java
+++ Bean_Scope/example7/src/main/java/com/example/beans/Person.java
@@ -1,11 +1,19 @@
 package com.example.beans;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+/* This is EAGER Instantiation of Singleton bean, where the bean is created
+ when the application context is created before the bean is called or used. */
+
+/* This is LAZY Instantiation of Singleton bean, where the bean is created
+ when the bean is called or used. This can be used to create beans which aren't used often. */
+
 @Component(value="person")
 @Scope(BeanDefinition.SCOPE_SINGLETON)
+@Lazy
 public class Person {
     private String name = "John";
 
--- Bean_Scope/example7/src/main/java/com/example/main/Example7.java
+++ Bean_Scope/example7/src/main/java/com/example/main/Example7.java
@@ -11,8 +11,10 @@ but only one instance will be returned.
 public class Example7 {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        System.out.println("The print statement before this is executed through EAGER Instantiation");
         Person p1 = context.getBean(Person.class);
         Person p2 = context.getBean("person", Person.class);
+        System.out.println("The print statement before this is executed through LAZY Instantiation");
         System.out.println("The hashcode for object p1 is " + p1.hashCode());
         System.out.println("The hashcode for object p2 is " + p2.hashCode());
         if(p1.hashCode()==p2.hashCode()){
