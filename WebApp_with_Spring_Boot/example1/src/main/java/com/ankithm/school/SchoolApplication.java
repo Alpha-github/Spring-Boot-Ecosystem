74e0033fb1a1b5a401940099e472638b109e19c5
Ankith M <reachankit2013@gmail.com>
2025-05-24 14:48:23 +0530

    feat: configure custom ports and context-path| handle autoconfiguration

--- WebApp_with_Spring_Boot/example1/src/main/java/com/ankithm/school/SchoolApplication.java
+++ WebApp_with_Spring_Boot/example1/src/main/java/com/ankithm/school/SchoolApplication.java
@@ -2,9 +2,12 @@ package com.ankithm.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 // @SpringBootApplication indicates the main class from which the
 // initialization/bootstrap of the application starts.
+
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,})
 @SpringBootApplication
 public class SchoolApplication {
 
