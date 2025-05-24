3e05784b929361b89e1e1d5957ed656799279472
Ankith M <reachankit2013@gmail.com>
2025-05-24 16:17:38 +0530

    feat: school webapp with thymeleaf template and devtools

---
+++ WebApp_with_Spring_Boot/example2/src/main/java/com/ankithm/school/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.ankithm.school.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    // When there is a request for a particular webpage like "/home", this method will be called.
+    @RequestMapping(value = {"","/","/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username",  "John Smith");
+        return "home.html";
+    }
+}
