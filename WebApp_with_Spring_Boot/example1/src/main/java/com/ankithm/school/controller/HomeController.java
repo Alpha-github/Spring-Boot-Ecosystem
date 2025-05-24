c7dced92f768729e57f768a9c3bcd9c40daca49e
Ankith M <reachankit2013@gmail.com>
2025-05-24 15:06:22 +0530

    feat: mapping multiple paths to single controller

--- WebApp_with_Spring_Boot/example1/src/main/java/com/ankithm/school/controller/HomeController.java
+++ WebApp_with_Spring_Boot/example1/src/main/java/com/ankithm/school/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     // When there is a request for a particular webpage like "/home", this method will be called.
-    @RequestMapping("/home")
+    @RequestMapping(value = {"","/","/home"})
     public String displayHomePage() {
         return "home.html";
     }
