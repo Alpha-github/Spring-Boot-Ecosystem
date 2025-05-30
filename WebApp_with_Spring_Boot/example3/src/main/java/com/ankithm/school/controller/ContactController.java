eda743ecedab4bc0c45242ca2b28fdd256b3bc14
Ankith M <reachankit2013@gmail.com>
2025-05-31 02:08:55 +0530

    feat: sSubmit info from Contact page using POJO object| introduce service and controller layers

--- WebApp_with_Spring_Boot/example3/src/main/java/com/ankithm/school/controller/ContactController.java
+++ WebApp_with_Spring_Boot/example3/src/main/java/com/ankithm/school/controller/ContactController.java
@@ -1,7 +1,10 @@
 package com.ankithm.school.controller;
 
+import com.ankithm.school.model.Contact;
+import com.ankithm.school.service.ContactService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,21 +17,35 @@ public class ContactController {
 
     private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
     @RequestMapping("/contact")
     public String displayContactPage() {
         return "contact.html";
     }
 
-    @RequestMapping(value = "/saveMsg", method = POST)
-    public ModelAndView saveMsg(@RequestParam String name, @RequestParam String mobileNum,
-                                @RequestParam String email, @RequestParam String subject,
-                                @RequestParam String message) {
-        log.info("Name: " + name);
-        log.info("Mobile Number: " + mobileNum);
-        log.info("Email: " + email);
-        log.info("Subject: " + subject);
-        log.info("Message: " + message);
-        return new ModelAndView("redirect:/contact");
+//    @RequestMapping(value = "/saveMsg", method = POST)
+//    public ModelAndView saveMsg(@RequestParam String name, @RequestParam String mobileNum,
+//                                @RequestParam String email, @RequestParam String subject,
+//                                @RequestParam String message) {
+//        log.info("Name: " + name);
+//        log.info("Mobile Number: " + mobileNum);
+//        log.info("Email: " + email);
+//        log.info("Subject: " + subject);
+//        log.info("Message: " + message);
+//        return new ModelAndView("redirect:/contact");
+//
+//    }
 
+    @RequestMapping(value = "/saveMsg", method = POST)
+    public ModelAndView saveMsg(Contact contact){
+        log.info(contact.toString());
+        contactService.saveMessageDetails(contact);
+        return new ModelAndView("redirect:/contact");
     }
 }
